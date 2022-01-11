f9710b75c120c7746123f52a73720c4bbafd4fcf
Fernando Saraiva <fpsaraiva@gmail.com>
2022-01-11 13:30:41 -0300

    cadastro + auth deliveryman

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanController.ts
@@ -0,0 +1,16 @@
+import { AuthenticateDeliverymanUseCase } from './AuthenticateDeliverymanUseCase';
+import { Request, Response } from 'express';
+
+export class AuthenticateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateDeliverymanUseCase = new AuthenticateDeliverymanUseCase();
+    const result = await authenticateDeliverymanUseCase.execute({
+      username,
+      password
+    });
+
+    return response.json(result);
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,5 @@
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from 'express';
@@ -6,8 +8,13 @@ const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+
+routes.post('/client/authenticate', authenticateClientController.handle);
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client', createClientController.handle);
-routes.post('/authenticate', authenticateClientController.handle);
+routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
