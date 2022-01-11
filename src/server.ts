f7c2bfb472a1382c6f630796a46dd59424b840b2
Fernando Saraiva <fpsaraiva@gmail.com>
2022-01-10 22:38:32 -0300

    tratamento de exceções

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 const app = express();
@@ -6,11 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello there!'
-  })
-})
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      message: err.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(3000, () => 
   console.log('Servidor está no AR!')
