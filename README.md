6cdbffcaf4580f6568dd9930ff868da1a8b79aaf
Fernando Saraiva <fpsaraiva@gmail.com>
2022-01-11 14:10:05 -0300

    cadastro entregas

--- README.md
+++ README.md
@@ -13,5 +13,8 @@ Projeto de um backend de entregas (delivery), realizado na trilha Node do Ignite
 
 ### Endpoints
 
-POST /client - criação de um novo cliente
-POST /authenticate - autentica usuário
+POST /client - criação de novo cliente  
+POST /client/authenticate - autentica cliente  
+POST /deliveryman - criação de novo entregador  
+POST /deliveryman/authenticate - autentica entregador  
+POST /delivery - criação de nova entrega  
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
@@ -10,11 +11,13 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+routes.post('/delivery', createDeliveryController.handle);
 
 export { routes };
